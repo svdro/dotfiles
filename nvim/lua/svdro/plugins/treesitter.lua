490b232a6c2d5df5537294f229c8fe14604141a2
svdro <sdroste@protonmail.com>
2024-04-28 22:01:23 +0200

    enable syntax highlighting for code in markdown files via treesitter

--- nvim/lua/svdro/plugins/treesitter.lua
+++ nvim/lua/svdro/plugins/treesitter.lua
@@ -8,31 +8,36 @@ return {
   config = function()
     local treesitter = require("nvim-treesitter.configs")
 
-    treesitter.setup( {
-      highlight = { enable = true, }, -- enable syntax highlighting
-      indent = { enable = true, },    -- enable indentation
-      autotag = { enable = true, },   -- enable 'nvim-ts-autotag' plugin
+    -- disable diagnostics: Missing required fields in type `TSConfig`: `modules`, `sync_install`, `ignore_install`, `auto_install`
+    ---@diagnostic disable-next-line: missing-fields
+    treesitter.setup({
+      highlight = { enable = true }, -- enable syntax highlighting
+      indent = { enable = true }, -- enable indentation
+      autotag = { enable = true }, -- enable 'nvim-ts-autotag' plugin
       ensure_installed = {
         "c",
         "dockerfile",
         "gitignore",
         "go",
         "json",
-        "lua", 
+        "lua",
+        "markdown",
+        "markdown_inline",
         "python",
         "vim",
         "vimdoc",
         "query",
       },
+      additional_vim_regex_highlighting = false, -- additional vim regex highlighting
       incremental_selection = {
         enable = true,
-	keymaps = {
-	  init_selection = "<leader>k",  	-- init incremental selection
-	  node_incremental = "<leader>k", 	-- expand to next increment
-	  scope_incremental = "<leader>l", 	-- expand to next increment (sometimes expands more)
-	  node_decremental = "<leader>j", 	-- shrink to prev increment
-	},
+        keymaps = {
+          init_selection = "<leader>k", -- init incremental selection
+          node_incremental = "<leader>k", -- expand to next increment
+          scope_incremental = "<leader>l", -- expand to next increment (sometimes expands more)
+          node_decremental = "<leader>j", -- shrink to prev increment
+        },
       },
-    } )
+    })
   end,
 }
